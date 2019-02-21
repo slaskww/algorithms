ff9bec806e6e65f1f5df33cc04dd9cfd6d1a0d85
KrzychuJedi <tharealiestjedi@gmail.com>
2019-02-21 20:25:22 +0100

    stack tests

--- src/test/java/org/sda/algorithms/exercices/StackTest.java
+++ src/test/java/org/sda/algorithms/exercices/StackTest.java
@@ -1,6 +1,5 @@
 package org.sda.algorithms.exercices;
 
-
 import org.junit.jupiter.api.Test;
 import org.sda.algorithms.exercises.Stack;
 
@@ -16,5 +15,48 @@ public class StackTest {
         stack.push(integer);
 
         assertThat(stack.size()).isEqualTo(1);
+    }
+
+    @Test
+    public void testGettingObjectFromStack(){
+        Integer integer = 34;
+        String someValue = "value";
+
+        Stack stack = new Stack();
+        stack.push(integer);
+        stack.push(someValue);
+
+        assertThat(stack.size()).isEqualTo(2);
+
+        Object result = stack.pop();
+
+        assertThat(result).isEqualTo(someValue);
+        assertThat(stack.size()).isEqualTo(1);
+    }
+
+    @Test
+    public void testFull(){
+        Stack stack = new Stack();
+
+        for(int i=0; i<10; i++){
+            stack.push(i);
+        }
+
+        assertThat(stack.size()).isEqualTo(10);
+        assertThat(stack.isFull()).isEqualTo(true);
+
+    }
+
+    @Test
+    public void testAddMoreThanStackSize(){
+        Stack stack = new Stack();
+
+        for(int i=0; i<11; i++){
+            stack.push(i);
+        }
+
+        assertThat(stack.size()).isEqualTo(10);
+        assertThat(stack.isFull()).isEqualTo(true);
+
     }
 }
