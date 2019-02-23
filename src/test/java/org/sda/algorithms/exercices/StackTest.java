42e3536f33c3eb2e1e8ffc0ca6c2618f17866b8b
KrzychuJedi <tharealiestjedi@gmail.com>
2019-02-23 20:56:26 +0100

    opisy

--- src/test/java/org/sda/algorithms/exercices/StackTest.java
+++ src/test/java/org/sda/algorithms/exercices/StackTest.java
@@ -7,55 +7,97 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class StackTest {
 
-    @Test
-    public void testStackSize(){
-        Integer integer = 34;
 
+    @Test // annotacja mówiąca że to będzie metoda testowa
+    public void testStackSize(){
+        // W tym teście sprawdzać będziemy czy rozmiar stacka jest prawidłowy po przeprowadzonych na nim operacjach.
+
+        // GIVEN - określa warunki początkowe
+        // Przygotowywujemy daną testówą w tym przypadku zmienną typu Integer o wartości 34.
+        Integer integer = 34;
+        // Przygotowywujemy obiekt typu Stack() -> Stos -> który będzie testowany
         Stack stack = new Stack();
+
+        // WHEN - opisuje akcje
+        // Wykonujemy akcję -> Dodajemy do stosu zmienną typu integer, powiększamy stack poprzez dodanie do niego naszej zmiennej.
         stack.push(integer);
 
+        // THEN - informuje o oczekiwanych rezultatach
+        // Sprawdzamy CZY -> assertThat -> rozmiar stacka jest równy -> isEqualTo -> 1. Jeśli tak, to true i test wykona się poprawnie.
         assertThat(stack.size()).isEqualTo(1);
     }
 
     @Test
     public void testGettingObjectFromStack(){
-        Integer integer = 34;
-        String someValue = "value";
+        // W tym teście sprawdzać będziemy czy wyjęta ze stacka wartość to ta z wierzchu
+        // i czy rozmiar stacka zostaje pomniejszony o ilość wyjętych wartości.
 
+        // GIVEN -> Przygotowywujemy
+        // Zmienna typu Integer równa 34
+        Integer integer = 34;
+        // Zmienna typu string równa "value"
+        String someValue = "value";
+        // Stack który będziemy testować
         Stack stack = new Stack();
+
+        // WHEN -> Wykonujemy akcję
+        // Dodajemy do stacka zmienną integer.
         stack.push(integer);
+        // Dodajemy do stacka zmienną someValue
         stack.push(someValue);
 
+        // THEN sprawdzamy czy rozmiar stacka jest równy 2! potrzebne do wykonania testu.
         assertThat(stack.size()).isEqualTo(2);
 
+        // WHEN Tworzymy nowy obiekt wyjmując ostatnią włożoną do stacka wartość -> w naszym przypadku to będzie someValue
         Object result = stack.pop();
 
+        // THEN
+        // Sprawdzamy czy wyjęta ze stacka wartość równa jest zmiennej someValue -> Tą ostatnią włożyliśmy
         assertThat(result).isEqualTo(someValue);
+        // Sprawdzamy czy rozmiar naszego stacka jest równy jeden, ponieważ usunęliśmy z niego jedną wartość poprzez pop.
         assertThat(stack.size()).isEqualTo(1);
     }
 
     @Test
     public void testFull(){
+        // Ten test za zadanie ma sprawdzenie czy Stack prawidłowo obsługuje zwrócenie informacji o tym czy jest pełny.
+        // STACK ma mieścić maksymalnie 10 wartości.
+
+        // GIVEN -> Obiekt stacka który będziemy testować
         Stack stack = new Stack();
 
+        // WHEN -> Petlą for wypełniamy Stack 10 wartościami
         for(int i=0; i<10; i++){
             stack.push(i);
         }
 
+        // THEN
+        // Sprawdzamy czy rozmiar stacka jest równy 10 -> Tyle wartości włożyliśmy do niego.
         assertThat(stack.size()).isEqualTo(10);
+        // Sprawdzamy czy dla stacka maksymalna ilość wartości to ta którą go wypełniliśmy w tym teście
+        // Porównując czy metoda isFull() zwraca true
         assertThat(stack.isFull()).isEqualTo(true);
 
     }
 
     @Test
     public void testAddMoreThanStackSize(){
+        // Test sprawdza czy jest możliwość dodania więcej niż MAX -> 10 wartości do stacka.
+
+        // GIVEN -> Obiekt stacka do testów
         Stack stack = new Stack();
 
+        // WHEN -> Próbujemy wypełnić STACK 11 wartościami.
         for(int i=0; i<11; i++){
             stack.push(i);
         }
 
+        // THEN
+        // Sprawdzamy czy rozmiar stacka jest równy 10 -> Tyle wartości maksymalnie może być przechowywane w Stacku.
         assertThat(stack.size()).isEqualTo(10);
+        // Sprawdzamy czy dla stacka maksymalna ilość wartości została osiągnięta
+        // Porównując czy metoda isFull() zwraca true
         assertThat(stack.isFull()).isEqualTo(true);
 
     }
