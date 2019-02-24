63fbbb980b587095a46dd987d8e14cf86eebc769
Krzysztof Gonia <krzysztof.gonia@globallogic.com>
2019-02-24 13:32:13 +0100

    inkrementacja tablice

---
+++ src/main/java/org/sda/algorithms/exercises/Incrementation.java
@@ -0,0 +1,15 @@
+package org.sda.algorithms.exercises;
+
+public class Incrementation {
+
+    public static void main(String[] args){
+        int i = 0;
+
+        i++;
+        System.out.println(i); // 1
+        System.out.println(i++); // 1
+        System.out.println(++i); // 3
+        System.out.println(i--); // 3
+        System.out.println(--i); // 1
+    }
+}
---
+++ src/main/java/org/sda/algorithms/exercises/Tablice.java
@@ -0,0 +1,15 @@
+package org.sda.algorithms.exercises;
+
+public class Tablice {
+
+    public static void main(String[] args) {
+        int[] tablica = new int[2];
+
+        tablica[0] = 1;
+        tablica[1] = 5;
+
+        double[] tablica2 = new double[5];
+
+        System.out.println(tablica[0]);
+    }
+}
