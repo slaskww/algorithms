fef32d38ba7cf7332a88bd9f9ce89ba6206e1806
lukasz <slaskww@gmail.com>
2019-02-28 21:55:39 +0100

    implemented Queue methods using LinkedList<Object> and its methods

--- src/main/java/org/sda/algorithms/exercises/Queue.java
+++ src/main/java/org/sda/algorithms/exercises/Queue.java
@@ -1,20 +1,54 @@
 package org.sda.algorithms.exercises;
 
+import java.util.LinkedList;
+
 public class Queue {
 
-    public Queue(){}
+    public static final int MAX_SIZE = 10;
+    private LinkedList<Object> queue;
 
-    public void  enqueue(Object o){}
+    public Queue() {
 
-    public Object dequeue(){ return null;}
+        queue = new LinkedList<>();
+    }
 
-    public Object peek(){return null;}
+    public void enqueue(Object o) {
 
-    public boolean isFull(){return false;}
+        if (queue.size() == MAX_SIZE) {
+            System.out.println("Queue is full");
+            return;
+        }
+        queue.offer(o);
 
-    public boolean isEmpty(){return false;}
+    }
 
-    public int size(){return 0;}
+    public Object dequeue() {
 
+        if (queue.size() == 0) {
+            System.out.println("Queue is empty");
+            return null;
+        }
 
+        return queue.remove();
+    }
+
+    public Object peek() {
+
+        return queue.peek();
+    }
+
+    public boolean isFull() {
+
+        return queue.size() == MAX_SIZE;
+    }
+
+    public boolean isEmpty() {
+
+        return queue.isEmpty();
+    }
+
+    public int size() {
+
+        return queue.size();
+    }
 }
