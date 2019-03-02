582d0631faaee0f2615e296191f833cc23c43811
lukasz <slaskww@gmail.com>
2019-03-02 10:48:16 +0100

    Edit Exercise 3

--- src/main/java/org/sda/algorithms/exercises/Exercise3.java
+++ src/main/java/org/sda/algorithms/exercises/Exercise3.java
@@ -3,6 +3,8 @@ package org.sda.algorithms.exercises;
 
 import org.sda.algorithms.util.CalcTime;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 /**
@@ -24,12 +26,28 @@ public class Exercise3 {
 
         // TODO
         // Pobierze od uzytkownika informacje ile przykladow chce sprawdzic
-        // Pobierz liczny
+        System.out.println("Ile liczb chcesz sprawdzić");
+        int numbers = scanner.nextInt();
+
+        // Pobierz liczby
+
+        List<Integer> numbersToTest = new ArrayList<Integer>();
+
+        for (int i = 0; i<numbers; i++) {
+            System.out.println("Podaj liczbe: ");
+            int n  =  scanner.nextInt();
+            numbersToTest.add(n);
+        }
 
         CALC_TIME.start();
 
         // TODO
         // Sprawdz czy liczba jest pierwsza uzywajac metody isPrime
+
+        for (int i: numbersToTest) {
+            System.out.print(i + ": ");
+            System.out.println(isPrime(i));
+        }
 
         CALC_TIME.stop();
         CALC_TIME.display();
@@ -39,6 +57,29 @@ public class Exercise3 {
     // zaimplementuj metode ktora zwroci true dla kazdej liczby pierwszej
     // i false dla innych
     public static boolean isPrime(int number) {
-        return false;
+
+        double sqrtNumb = Math.sqrt(number);
+
+        if (number ==  1){
+            return false;
+        }
+
+        if (number  ==  2){
+
+            return true;
+        }
+
+
+        if (number % 2 == 0){
+            return false;
+        }
+
+        for (int i = 3; i <= sqrtNumb; i+=2 ){
+
+                  if (number % i == 0)
+                      return false;
+                }
+
+        return true;
     }
 }
