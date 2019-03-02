b8e2a4ef9747f4f3f27ea7fc7fb77d861f226393
lukasz <slaskww@gmail.com>
2019-03-02 09:02:30 +0100

    merge

--- src/main/java/org/sda/algorithms/exercises/Stack.java
+++ src/main/java/org/sda/algorithms/exercises/Stack.java
@@ -1,11 +1,6 @@
 package org.sda.algorithms.exercises;
 
 public class Stack {
-    public final int DEFAULT_MAX_SIZE = 10;
-    private Object[] stack;
-    private int positionOfElement = 0;
-    private int maxSize;
-
 
     private int maxSize = 10;
     // tablica w której będziemy trzymać obiekty
@@ -14,19 +9,8 @@ public class Stack {
     private int size = 0;
 
     public Stack() {
-
-        maxSize =  DEFAULT_MAX_SIZE;
-        stack = new Object[maxSize];
     }
 
-    public Stack(int s) {
-
-        maxSize = s;
-        stack = new Object[maxSize];
-
-    }
-
-<<<<<<< HEAD
     // Dodatkowo:
     // Drugi kontruktor który bedzie przyjmowal rozmiar stosu
 
@@ -85,52 +69,6 @@ public class Stack {
 
     public int size(){
         return size;
-=======
-
-    public void push(Object o){
-
-        if (positionOfElement == maxSize){
-            System.out.println("przekroczyłeś dopuszczalną wielkość stosu");
-            return;
-        }
-        stack[positionOfElement]  = o;
-        positionOfElement++;
-
-    }
-
-    public Object pop(){
-        if (positionOfElement != 0){
-            positionOfElement--;
-           Object o =  stack[positionOfElement];
-            stack[positionOfElement] = null;
-            return o;
-        }
-        System.out.println("brak elementów na stosie");
-        return null;
-
-    }
-
-    public Object peek(){
-
-        if (positionOfElement != 0){
-            return stack[positionOfElement-1];
-        }
-        System.out.println("brak elementów na stosie");
-        return null;
-    }
-
-    public boolean isFull(){
-
-        return (stack.length == maxSize);
-    }
-
-    public boolean isEmpty(){
-        return  (stack.length == 0);
-    }
-
-    public int size(){
-        return positionOfElement;
->>>>>>> myBranch
     }
 
 }
