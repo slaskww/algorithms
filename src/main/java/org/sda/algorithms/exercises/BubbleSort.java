f4d63f2da662c99983b13f811d52b7b3f5651b84
lukasz <slaskww@gmail.com>
2019-03-03 16:01:57 +0100

    Add BubbleSort algo

--- src/main/java/org/sda/algorithms/exercises/BubbleSort.java
+++ src/main/java/org/sda/algorithms/exercises/BubbleSort.java
@@ -1,7 +1,10 @@
 package org.sda.algorithms.exercises;
 
+import java.awt.*;
 import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Random;
 
 public class BubbleSort {
@@ -9,37 +12,50 @@ public class BubbleSort {
 
     public static void main(String[] args) {
 
-        Random rand = new Random();
+
+
+       /* Random rand = new Random();
         int[] arrayToSort = new int[10];
 
+        System.out.print("[");
         for (int i = 0; i < arrayToSort.length ; i++) {
 
             arrayToSort[i] = rand.nextInt()% 100;
-            System.out.println(arrayToSort[i]);
+            System.out.print(arrayToSort[i] + " ");*/
+
+        Random rand = new Random();
+
+        ArrayList<House> houses = new ArrayList<>();
+
+        for (House h: houses) {
+            h = new House(120 +  rand.nextInt()% 50, 70 +  rand.nextInt()% 50, 1 +  rand.nextInt()% 5);
         }
 
-        sortBubble(arrayToSort);
+
+        sortBubble(houses);
     }
 // 1  -3  7  2  -5
+//-5  -3  1  2   7
 
-  public static void sortBubble(int[] array){
+  public static void sortBubble(ArrayList<House>[] arrays){
 
-      for (int i = 0; i < array.length - 1; i++) {
+      for (int i = 0; i < arrays.length - 1; i++) {
 
 
-          for (int j = 0; j < array.length-1 ; j++) {
+          for (int j = 0; j < arrays.length -1 ; j++) {
 
               int temp;
 
-              if (array[j] > array[j+1]){
+              if (arrays. > array[j+1]){
 
                   temp = array[j];
-                  array[j]= array[j+1];
-                  array[j+1]= temp;
+                  array[j] = array[j+1];
+                  array[j+1] = temp;
 
               }
           }
       }
+
       System.out.println(Arrays.toString(array));
   }
 
---
+++ src/main/java/org/sda/algorithms/exercises/House.java
@@ -0,0 +1,15 @@
+package org.sda.algorithms.exercises;
+
+public class House {
+
+   private   double price;
+   private  double size;
+   private int rooms;
+
+
+    public House(double price, double size, int rooms) {
+        this.price = price;
+        this.size = size;
+        this.rooms = rooms;
+    }
+}
