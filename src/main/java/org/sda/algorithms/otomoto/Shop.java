22f5a55ab98c32a5fe49900fa86aa3fb51c259e4
lukasz <slaskww@gmail.com>
2019-03-10 10:26:34 +0100

    Add class Car

---
+++ src/main/java/org/sda/algorithms/otomoto/Shop.java
@@ -0,0 +1,30 @@
+package org.sda.algorithms.otomoto;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Shop {
+
+    public static void main(String[] args) {
+
+        List<Car> cars = new ArrayList<>();
+
+       /* Address address  =  new Address("Wiejska","55-435", "Wroclaw", "13/12");
+        Owner owner = new Owner("Jan", "Noga", address);
+        Car car = new Car("9000", "green", "C-01", "Toyota", true, owner);
+
+        cars.add(car);
+
+        String cityResult = cars.get(0).getOwner().getAddress().getCity();
+
+        Car someCars = cars.get(0);
+        Owner someOwner = someCars.getOwner();
+        Address someAddress = someOwner.getAddress();
+        String someCity = someAddress.getCity();
+
+*/
+
+
+    }
+
+}
