945a814379f22f923daefc7166b3983e8133eb84
lukasz <slaskww@gmail.com>
2019-03-14 22:50:43 +0100

    Edit class ListOfNumbers

--- src/main/java/org/sda/algorithms/exceptions/ListOfNumbers.java
+++ src/main/java/org/sda/algorithms/exceptions/ListOfNumbers.java
@@ -1,6 +1,7 @@
 package org.sda.algorithms.exceptions;
 
 import java.io.FileWriter;
+import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.List;
@@ -10,7 +11,7 @@ public class ListOfNumbers {
     private List<Integer> list;
     private static final int SIZE = 10;
 
-    public ListOfNumbers () {
+    public ListOfNumbers() {
         list = new ArrayList<Integer>(SIZE);
         for (int i = 0; i < SIZE; i++) {
             list.add(new Integer(i));
@@ -19,12 +20,46 @@ public class ListOfNumbers {
 
     public void writeList() {
         // The FileWriter constructor throws IOException, which must be caught.
-        PrintWriter out = new PrintWriter(new FileWriter("OutFile.txt"));
+        PrintWriter out = null;
 
-        for (int i = 0; i < SIZE; i++) {
-            // The get(int) method throws IndexOutOfBoundsException, which must be caught.
-            out.println("Value at: " + i + " = " + list.get(i));
+        try {
+            System.out.println("Entering try statement");
+            out = new PrintWriter(new FileWriter(" "));
+            for (int i = 0; i < SIZE+20; i++) {
+                // The get(int) method throws IndexOutOfBoundsException, which must be caught.
+                out.println("Value at: " + i + " = " + list.get(i));
+            }
+
+        } catch (IOException e) {
+            System.err.println("Caught  IOException " + e.getMessage());
+            e.printStackTrace();
+
+        } catch (IndexOutOfBoundsException e) {
+            System.err.printf("getLocalizedMessage %20s", e.getLocalizedMessage());
+            System.err.println("getMessage " + e.getMessage());
+            System.err.println("toString " + e.toString());
+            System.err.println("fillInStackTrace " + e.fillInStackTrace());
+            System.err.println("getCause " + e.getCause());
+            System.err.println("getStackTrace " + e.getStackTrace());
+            System.err.println("getSuppressed " + e.getSuppressed());
+            System.err.println("hashCode " + e.hashCode());
+            System.err.println("getClass " + e.getClass());
+            e.printStackTrace();
+
+        } finally {
+            if (out != null) {
+                out.close();
+                System.out.println("Closing PrintWriter");
+            } else {
+                System.out.println("PrintWriter not open");
+            }
         }
-        out.close();
     }
+
+    public static void main(String[] args) {
+
+        ListOfNumbers list = new ListOfNumbers();
+        list.writeList();
+    }
+
 }
