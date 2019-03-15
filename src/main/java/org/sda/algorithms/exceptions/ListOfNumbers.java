595b821d9bf06e546c1b5c2e6fa3044e1782ba70
lukasz <slaskww@gmail.com>
2019-03-15 22:15:07 +0100

    edit ListOfNumbers class

--- src/main/java/org/sda/algorithms/exceptions/ListOfNumbers.java
+++ src/main/java/org/sda/algorithms/exceptions/ListOfNumbers.java
@@ -5,6 +5,10 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.FileHandler;
+import java.util.logging.Handler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class ListOfNumbers {
 
@@ -23,16 +27,26 @@ public class ListOfNumbers {
         PrintWriter out = null;
 
         try {
+            Handler handler = new FileHandler("OutFile.log");
+            Logger.getLogger("").addHandler(handler);
             System.out.println("Entering try statement");
-            out = new PrintWriter(new FileWriter(" "));
+            out = new PrintWriter(new FileWriter(""));//OutFile.txt"));
             for (int i = 0; i < SIZE+20; i++) {
                 // The get(int) method throws IndexOutOfBoundsException, which must be caught.
                 out.println("Value at: " + i + " = " + list.get(i));
+
             }
 
         } catch (IOException e) {
             System.err.println("Caught  IOException " + e.getMessage());
-            e.printStackTrace();
+           // e.printStackTrace();
+            e.getStackTrace();
+            Logger logger = Logger.getLogger("package.pl");
+            StackTraceElement elements[] = e.getStackTrace();
+            for (int i = 0, n = elements.length; i < n; i++) {
+                logger.log(Level.WARNING, elements[i].getMethodName());
+            }
+
 
         } catch (IndexOutOfBoundsException e) {
             System.err.printf("getLocalizedMessage %20s", e.getLocalizedMessage());
